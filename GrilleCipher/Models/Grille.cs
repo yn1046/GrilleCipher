d45344487c6c34ca9b1040a6d8390569a0c4d98b
yn1046 <wantedblood@2ch.hk>
2018-03-02 00:15:45 +0300

    basic markup written

---
+++ GrilleCipher/Models/Grille.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrilleCipher.Models
+{
+    public class Grille
+    {
+        bool[,] BooleanMatrix { get; set; } = new bool[3, 3];
+    }
+}
